ab593a99e4208884f4fb7a77a0d34e059a18583c
TemryL <tommery01@gmail.com>
2024-08-27 19:25:00 -0400

    rename cfg files

---
+++ configs/predict_cfg_expanded.py
@@ -0,0 +1,32 @@
+# Data:
+batch_size = 256
+num_features = [
+    'BMI',
+    'HDL cholesterol',
+    'LDL cholesterol',
+    'Total cholesterol',
+    'Triglycerides',
+    'Diastolic blood pressure'
+]
+cat_features = [
+    'Age',
+    'Sex',
+    'Ever smoked',
+    'Snoring',
+    'Insomnia',
+    'Daytime napping',
+    'Chronotype',
+    'Sleep duration',
+]
+diseases = [
+    'Asthma',
+    'Cataract',
+    'Diabetes',
+    'GERD',
+    'Hay-fever & Eczema',
+    'Major depression',
+    'Myocardial infarction',
+    'Osteoarthritis',
+    'Pneumonia',
+    'Stroke'
+]
